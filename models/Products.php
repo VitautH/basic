82b490c0fad099934ccac42a2a081b77a4274cc9
VitautH <ip-94@ya.ru>
2017-03-20 18:10:16 +0300

    Добавлен метод  проверки полей Добавлен метод получения массива казино

--- models/Products.php
+++ models/Products.php
@@ -4,9 +4,29 @@ namespace app\models;
 
 use Yii;
 use app\models\base\Products as BaseProducts;
-
+use yii\helpers\ArrayHelper;
 
 class Products extends BaseProducts
 {
 
+    /*
+     * Проверка на ввод данных + обязательные поля
+     */
+    public function rules()
+    {
+        return [[['title','cost','casino_id'],'required'],
+            [['casino_id', 'cost', 'cashback'],'number'],
+           [['title', 'description', 'meta_keywords', 'meta_description'],'safe'],
+        ];
+    }
+
+    /*
+     * @return Array CasinoList
+     *
+     */
+    public function getCasinoList()
+    {
+ $casino_array = casino::find()->select(['id','title'])->all();
+       return $items = ArrayHelper::map($casino_array,'id','title');
+    }
 }
