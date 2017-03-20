6b9489818c15c66834b3dda061c4836831fc6ddd
VitautH <ip-94@ya.ru>
2017-03-20 13:26:47 +0300

    Сделан редирект на страницу login при доступе к админке не авторизованым пользователям. Закрыт доступ в админку по-умолчанию.

--- modules/admin/controllers/AdminController.php
+++ modules/admin/controllers/AdminController.php
@@ -9,16 +9,17 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use app\modules\controllers;
-/**
- * ProductsController implements the CRUD actions for Products model.
- */
+
 class AdminController extends Controller
 {
     public function beforeAction($action)
     {
-
+/*
+ *  Редирект на вход
+ */
         if (empty(Yii::$app->user->identity)) {
-            return false;
+          $this->redirect('/login');
+          return false;
         }
         return parent::beforeAction($action);
     }
