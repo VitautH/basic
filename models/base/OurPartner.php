be3d18960c7418506a91afdd7f9ad11bae01a35d
Григорович Витовт <ip-94@ya.ru>
2017-04-06 18:34:50 +0300

    Вывод модуля Наши Партнеры на главной странице TODo: Доделать обновление Наши Партнеры в Админке

---
+++ models/base/OurPartner.php
@@ -0,0 +1,42 @@
+<?php
+namespace app\models\base;
+
+use Yii;
+
+use yii\helpers\ArrayHelper;
+use app\core\ImageClass;
+use yii\web\UploadedFile;
+
+class OurPartner extends  \yii\db\ActiveRecord
+{
+    public $imageFile;
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'modules_our_partners';
+    }
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'url'], 'string'],
+           ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'url' => 'Url'
+        ];
+    }
+
+}
