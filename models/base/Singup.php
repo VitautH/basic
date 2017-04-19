8c7969caff12826abff3a0f669da13fe2379a852
Григорович Витовт <ip-94@ya.ru>
2017-04-19 18:06:56 +0300

    Начал делать кабинет пользователя

---
+++ models/base/Singup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitaut
+ * Date: 19.04.2017
+ * Time: 17:52
+ */
+
+namespace app\models\base;
+
+
+class Singup
+{
+
+}
--- modules/admin/views/coupon/index.php
+++ modules/admin/views/coupon/index.php
@@ -6,16 +6,14 @@ use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Новости';
+$this->title = 'Купоны';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="casino-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create ', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+
     <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
