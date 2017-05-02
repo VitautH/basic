118814977d5c10af989bcff320f34a1f08c0fb73
Григорович Витовт <ip-94@ya.ru>
2017-05-02 18:30:01 +0300

    Сделан контроллер заказа Делаю имитацию оплаты

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -4,12 +4,21 @@ namespace app\controllers;
 use app\controllers\Base\MainController as MainController;
 use app\models\User;
 use app\models\Order;
-use app\models\Coupon;
 use Yii;
 use  app;
 use yii\web\ForbiddenHttpException;
 class OrderController extends MainController
 {
+    private $user;
+    private $order;
+private $status;
+    public function __construct($id, \yii\base\Module $module, array $config = [])
+    {
+        $this->user =  new User();
+        $this->order= new Order();
+        parent::__construct($id, $module, $config);
+    }
+
     /*
      * Access Denided Guest
      */
@@ -17,8 +26,8 @@ class OrderController extends MainController
     public function beforeAction($action)
     {
 
-        $user = new User;
-if ($this->getRole()==$user::GUEST) {
+// ToDo: PHP 7 $this->user::GUEST; !
+if ($this->getRole()==User::GUEST) {
 
     throw new ForbiddenHttpException('Доступ запрещён');
 }
@@ -30,17 +39,67 @@ else {
 
     public function actionCreate()
     {
-$coupon =  new Coupon();
-$order = new Order();
-       var_dump(Yii::$app->request->post());
-        Yii::$app->end();
-       // return $this->render('create');
+
+
+        $this->order->scenario = 'create_order';
+        $request = Yii::$app->request;
+        if ($request->isPost) {
+
+            $this->order->load($request->post());
+
+            if ($this->order->save()) {
+                return $this->redirect(['order', 'view' => $this->order->id]);
+
+            } else {
+
+                throw new ForbiddenHttpException('Произошла ошибка!');
+            }
+
+        }
     }
 
+
+
+
+
+    //ToDo: вывод списка заказов для менеджера
     public function actionIndex()
     {
 
         //return $this->render('index');
+    }
+
+    // ToDo: переписать!
+public function actionView($id){
+    return $this->render('view', [
+        'model' => $this->findModel($id),
+    ]);
+
+}
+public function actionPayment()
+{
+    $request= Yii::$app->request;
+    if ($request->isPost) {
+if ($request->post()['status']== 1){
+    return $this->render('succes', [
+        //'model' => $this->findModel($id),
+    ]);
+}
+
+    }
+    else {
+        throw new ForbiddenHttpException('Произошла ошибка!');
+    }
+
+}
+
+    protected function findModel($id)
+    {
+        if (($model = Order::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new ForbiddenHttpException('The requested page does not exist.');
+        }
     }
 
 }
