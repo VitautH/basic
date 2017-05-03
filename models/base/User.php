5590492d97e6e5f503ff048f4bd0f63ab7eae489
Григорович Витовт <ip-94@ya.ru>
2017-05-03 18:24:46 +0300

    Новая версия User

--- models/base/User.php
+++ models/base/User.php
@@ -7,15 +7,30 @@ use Yii;
 /**
  * This is the model class for table "user".
  *
- * @property integer $user_id
- * @property string $login
- * @property string $pass
- * @property string $mail
+ * @property integer $id
+ * @property string $username
+ * @property string $email
  * @property string $phone
+ * @property string $name
+ * @property string $firstname
+ * @property string $lastname
  * @property integer $role_id
+ * @property string $password_hash
+ * @property string $auth_key
+ * @property integer $confirmed_at
+ * @property string $unconfirmed_email
+ * @property integer $blocked_at
+ * @property string $registration_ip
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $flags
+ * @property integer $last_login_at
  *
- * @property Role $role
- * @property UserPayment[] $userPayments
+ * @property Order[] $orders
+ * @property Profile $profile
+ * @property SocialAccount[] $socialAccounts
+ * @property Token[] $tokens
+ * @property UserRole $role
  */
 class User extends \yii\db\ActiveRecord
 {
@@ -33,9 +48,16 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['login', 'pass', 'mail', 'phone'], 'string'],
-            [['role_id'], 'integer'],
-            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::className(), 'targetAttribute' => ['role_id' => 'id_role']],
+            [['username', 'email', 'phone', 'name', 'firstname', 'lastname', 'password_hash', 'auth_key', 'created_at', 'updated_at'], 'required'],
+            [['role_id', 'confirmed_at', 'blocked_at', 'created_at', 'updated_at', 'flags', 'last_login_at'], 'integer'],
+            [['username', 'email', 'unconfirmed_email'], 'string', 'max' => 255],
+            [['phone', 'name', 'firstname', 'lastname'], 'string', 'max' => 225],
+            [['password_hash'], 'string', 'max' => 60],
+            [['auth_key'], 'string', 'max' => 32],
+            [['registration_ip'], 'string', 'max' => 45],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserRole::className(), 'targetAttribute' => ['role_id' => 'id']],
         ];
     }
 
@@ -45,29 +67,64 @@ class User extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'user_id' => 'User ID',
-            'login' => 'Login',
-            'pass' => 'Pass',
-            'mail' => 'Mail',
+            'id' => 'ID',
+            'username' => 'Username',
+            'email' => 'Email',
             'phone' => 'Phone',
+            'name' => 'Name',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
             'role_id' => 'Role ID',
+            'password_hash' => 'Password Hash',
+            'auth_key' => 'Auth Key',
+            'confirmed_at' => 'Confirmed At',
+            'unconfirmed_email' => 'Unconfirmed Email',
+            'blocked_at' => 'Blocked At',
+            'registration_ip' => 'Registration Ip',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'flags' => 'Flags',
+            'last_login_at' => 'Last Login At',
         ];
     }
-
-    // ToDo:  Переписать? Удалить?
-//    /**
-//     * @return \yii\db\ActiveQuery
-//     */
-//    public function getRole()
-//    {
-//        return $this->hasOne(Role::className(), ['id_role' => 'role_id']);
-//    }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-//    public function getUserPayments()
-//    {
-//        return $this->hasMany(UserPayment::className(), ['user_id' => 'user_id']);
-//    }
+    public function getOrders()
+    {
+        return $this->hasMany(Order::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProfile()
+    {
+        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSocialAccounts()
+    {
+        return $this->hasMany(SocialAccount::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTokens()
+    {
+        return $this->hasMany(Token::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRole()
+    {
+        return $this->hasOne(UserRole::className(), ['id' => 'role_id']);
+    }
 }
