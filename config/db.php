828c34e74e014ba0cf25ed95e0d05d9bb516750b
Григорович Витовт <ip-94@ya.ru>
2017-05-26 12:02:24 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=casino',
-    'username' => 'vitaut',
-    'password' => 'FONVORK',
-    'charset' => 'utf8',
-];
