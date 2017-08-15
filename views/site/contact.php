cc5f46f79bb9f5bf128026d5bf803d9dd8f71772
Григорович Витовт <ip-94@ya.ru>
2017-08-15 11:25:10 +0300

    Fixed css style

--- views/site/contact.php
+++ views/site/contact.php
@@ -26,65 +26,67 @@ $this->registerCssFile('/css/contact.css');
 ?>
 <div class="clearfix"></div>
 
-    <div class="container">
-        <div class="contact">
+<div class="container">
+    <div class="contact">
         <div class="row">
             <div class="col-lg-12 col-xs-12">
-                <h1><?=$this->title; ?></h1>
+                <h1><?= _t('Контакты'); ?></h1>
             </div>
         </div>
-            <div class="row">
-                <div class="col-lg-12 col-xs-12">
-                    <div class="maps">
+        <div class="row">
+            <div class="col-lg-12 col-xs-12">
+                <div class="maps">
 
-            <?
-            $coord = new LatLng(['lat' => 53.9272585, 'lng' =>  27.6671514]);
-            $map = new Map([
-                'center' => $coord,
-                'zoom' => 14,
-                'width'=>'100%',
-                  'height'=>'594'
-            ]);
-            // Lets configure the polyline that renders the direction
-            $polylineOptions = new PolylineOptions([
-                'strokeColor' => '#FFAA00',
-                'draggable' => true
-            ]);
+                    <?
+                    $coord = new LatLng(['lat' => 53.9272585, 'lng' => 27.6671514]);
+                    $map = new Map([
+                        'center' => $coord,
+                        'zoom' => 14,
+                        'width' => '100%',
+                        'height' => '594'
+                    ]);
+                    // Lets configure the polyline that renders the direction
+                    $polylineOptions = new PolylineOptions([
+                        'strokeColor' => '#FFAA00',
+                        'draggable' => true
+                    ]);
 
-            $waypoints = [
-                new DirectionsWayPoint(['location' => $coord])
-            ];
+                    $waypoints = [
+                        new DirectionsWayPoint(['location' => $coord])
+                    ];
 
-            // Lets add a marker now
-            $marker = new Marker([
-                'position' => $coord,
-                'title' => 'TIME GAME',
-            ]);
-            $map->addOverlay($marker);
+                    // Lets add a marker now
+                    $marker = new Marker([
+                        'position' => $coord,
+                        'title' => 'TIME GAME',
+                    ]);
+                    $map->addOverlay($marker);
 
-            echo $map->display();
+                    echo $map->display();
 
 
-
-            ?>
-                        <div class="contact_block">
-                            <img src="/image/mini_logo.png"/>
-                            <ul>
-                                <li  class="addres"><span class="glyphicon glyphicon-map-marker"></span>22040, Республика Беларусь, г. Минск, ул. Сурганова, 2А, ОФ. 3</li>
-                              <br>
-
-                                <li class="phone"><span class="glyphicon glyphicon-earphone"></span> +37529-755-55-55</li>
-                                <br>
-                                <li class="email"><span class="glyphicon glyphicon-envelope"></span> info@timegame.by</li>
-                                <br>
-                                <li class="skype"><i class="fa fa-skype"></i>timegame</li>
-                            </ul>
+                    ?>
+                    <div class="contact_block">
+                        <div>
+                            <img src="/image/mini_logo.png">
                         </div>
+                        <ul>
+                            <li class="addres"><span class="glyphicon glyphicon-map-marker"></span>22040, Республика
+                                Беларусь, г. Минск, ул. Сурганова, 2А, ОФ. 3
+                            </li>
+                            <br>
+
+                            <li class="phone"><span class="glyphicon glyphicon-earphone glyphicon-earphone-cont"></span>
+                                +37529-755-55-55
+                            </li>
+                            <br>
+                            <li class="email"><span class="glyphicon glyphicon-envelope"></span> info@timegame.by</li>
+                            <br>
+                            <li class="skype"><i class="fa fa-skype"></i>timegame</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
-
-
+</div>
